8b193ef11cc5fa7a2103b49abf4deca52ce4a2c2
Marta.Weiner <martawei19@gmail.com>
2022-07-30 18:29:30 +0200

    Add new files

--- src/com/exercise/Main.java
+++ src/com/exercise/Main.java
@@ -7,17 +7,19 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Attention. Enter the correct coordinates, if you make a mistake there is no return option.");
         Game game_init = new Game();
         game_init.iteration();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Do you want to play again? (only 'yes' answer is accepted as desire to continue)");
-        String answer = scanner.next();
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Do you want to play again? (only 'yes' answer is accepted as desire to continue)");
+            String answer = scanner.next();
 
-        while(answer.equals("yes")){
-            Game game = new Game();
-            game.iteration();
-            if (!answer.equals("yes")){
+            if(answer.equals("yes")){
+                Game game = new Game();
+                game.iteration();
+            } else {
                 break;
             }
         }
